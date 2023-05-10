b922c33ce3cb88bd6db24755a44c1903d0d35499
Jaqueline Cayo <gabcayjam@alu.edu.gva.es>
2023-05-10 12:04:13 +0200

    primer commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,4 @@
+<template>
+
+    <h1>Ancara Messi</h1>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+<h1>Mi casa</h1>
+<Jaque/>
+</template>
+
+<script setup>
+import Jaque from '../components/Nav.vue'
+</script>
